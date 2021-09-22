b218631d27ba8f188679853c835e9fe59f52a207
Xavier Negron <xaviernegron888@gmail.com>
2021-09-21 22:11:55 -0500

    Write helper functions

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -6,21 +6,24 @@ import {
   ethereum,
   log,
 } from "@graphprotocol/graph-ts";
-import { ERC20 } from "../types/Factory/ERC20";
-import { ERC20NameBytes } from "../types/Factory/ERC20NameBytes";
-import { ERC20SymbolBytes } from "../types/Factory/ERC20SymbolBytes";
+import { Pool as PoolContract} from "../../generated/templates/Pool/Pool";
+import { NFTPool as NFTPoolContract} from "../../generated/templates/NFTPool/NFTPool";
 import {
-  LiquidityPosition,
-  LiquidityPositionSnapshot,
-  Pair,
-  Token,
+  PoolPosition,
+  PoolPositionSnapshot,
+  NFTPoolPosition,
+  NFTPoolPositionSnapshot,
+  AssetPosition,
+  Pool,
+  NFTPool,
   User,
-} from "../types/schema";
-import { Factory as FactoryContract } from "../types/templates/Pair/Factory";
-import { CELO_ADDRESS } from "./pricing";
+} from "../../generated/schema";
+import { PoolFactory } from "../../generated/PoolFactory/PoolFactory";
+import { NFTPoolFactory } from "../../generated/NFTPoolFactory/NFTPoolFactory";
 
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
-export const FACTORY_ADDRESS = "0x62d5b84bE28a183aBB507E125B384122D2C25fAE";
+export const POOL_FACTORY_ADDRESS = "0xC71Ff58Efa2bffaE0f120BbfD7C64893aA20bDE0";
+export const NFT_POOL_FACTORY_ADDRESS = "0x2dB13ac7A21F42bcAaFC71C1f1F8c647AEBC9750";
 
 export let ZERO_BI = BigInt.fromI32(0);
 export let ONE_BI = BigInt.fromI32(1);
@@ -28,8 +31,12 @@ export let ZERO_BD = BigDecimal.fromString("0");
 export let ONE_BD = BigDecimal.fromString("1");
 export let BI_18 = BigInt.fromI32(18);
 
-export let factoryContract = FactoryContract.bind(
-  Address.fromString(FACTORY_ADDRESS)
+export let poolFactoryContract = PoolFactory.bind(
+  Address.fromString(POOL_FACTORY_ADDRESS)
+);
+
+export let NFTPoolFactoryContract = NFTPoolFactory.bind(
+  Address.fromString(NFT_POOL_FACTORY_ADDRESS)
 );
 
 export function exponentToBigDecimal(decimals: BigInt): BigDecimal {
@@ -45,7 +52,7 @@ export function bigDecimalExp18(): BigDecimal {
 }
 
 export function convertEthToDecimal(eth: BigInt): BigDecimal {
-  return eth.toBigDecimal().div(exponentToBigDecimal(18));
+  return eth.toBigDecimal().div(exponentToBigDecimal(new BigInt(18)));
 }
 
 export function convertTokenToDecimal(
@@ -74,144 +81,215 @@ export function isNullEthValue(value: string): boolean {
   );
 }
 
-export function fetchTokenSymbol(tokenAddress: Address): string {
-  // hard coded overrides
-  if (tokenAddress.toHexString() == CELO_ADDRESS) {
-    return "CELO";
-  }
+export function fetchPoolName(poolAddress: Address): string {
+  let contract = PoolContract.bind(poolAddress);
 
-  let contract = ERC20.bind(tokenAddress);
-  let contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress);
-
-  // try types string and bytes32 for symbol
-  let symbolValue = "unknown";
-  let symbolResult = contract.try_symbol();
-  if (symbolResult.reverted) {
-    let symbolResultBytes = contractSymbolBytes.try_symbol();
-    if (!symbolResultBytes.reverted) {
-      // for broken pairs that have no symbol function exposed
-      if (!isNullEthValue(symbolResultBytes.value.toHexString())) {
-        symbolValue = symbolResultBytes.value.toString();
-      }
-    }
-  } else {
-    symbolValue = symbolResult.value;
-  }
-
-  return symbolValue;
-}
-
-export function fetchTokenName(tokenAddress: Address): string {
-  // hard coded overrides
-  if (tokenAddress.toHexString() == CELO_ADDRESS) {
-    return "Celo Native Asset";
-  }
-
-  let contract = ERC20.bind(tokenAddress);
-  let contractNameBytes = ERC20NameBytes.bind(tokenAddress);
-
-  // try types string and bytes32 for name
+  // try types string for name
   let nameValue = "unknown";
   let nameResult = contract.try_name();
-  if (nameResult.reverted) {
-    let nameResultBytes = contractNameBytes.try_name();
-    if (!nameResultBytes.reverted) {
-      // for broken exchanges that have no name function exposed
-      if (!isNullEthValue(nameResultBytes.value.toHexString())) {
-        nameValue = nameResultBytes.value.toString();
-      }
-    }
-  } else {
-    nameValue = nameResult.value;
-  }
 
-  return nameValue;
+  return nameResult.value ? nameResult.value : nameValue;
 }
 
-export function fetchTokenTotalSupply(tokenAddress: Address): BigInt {
-  let contract = ERC20.bind(tokenAddress);
-  let totalSupplyValue = null;
+export function fetchPoolPerformanceFee(poolAddress: Address): BigInt {
+  let contract = PoolContract.bind(poolAddress);
+
+  let performanceFeeValue = new BigInt(0);
+  let performanceFeeResult = contract.try__performanceFee();
+
+  return performanceFeeResult.value ? performanceFeeResult.value : performanceFeeValue;
+}
+
+export function fetchPoolTokenPrice(poolAddress: Address): BigInt {
+  let contract = PoolContract.bind(poolAddress);
+
+  let tokenPriceValue = new BigInt(0);
+  let tokenPriceResult = contract.try_tokenPrice();
+
+  return tokenPriceResult.value ? tokenPriceResult.value : tokenPriceValue;
+}
+
+export function fetchPoolTotalSupply(poolAddress: Address): BigInt {
+  let contract = PoolContract.bind(poolAddress);
+
+  let totalSupplyValue = new BigInt(0);
   let totalSupplyResult = contract.try_totalSupply();
-  if (!totalSupplyResult.reverted) {
-    totalSupplyValue = totalSupplyResult as i32;
-  }
-  return BigInt.fromI32(totalSupplyValue as i32);
+
+  return totalSupplyResult.value ? totalSupplyResult.value : totalSupplyValue;
 }
 
-export function fetchTokenDecimals(tokenAddress: Address): BigInt {
-  // hardcode overrides
-  if (
-    tokenAddress.toHexString() == "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"
-  ) {
-    return BigInt.fromI32(18);
-  }
+export function fetchNFTPoolName(NFTPoolAddress: Address): string {
+  let contract = NFTPoolContract.bind(NFTPoolAddress);
 
-  let contract = ERC20.bind(tokenAddress);
-  // try types uint8 for decimals
-  let decimalValue = null;
-  let decimalResult = contract.try_decimals();
-  if (!decimalResult.reverted) {
-    decimalValue = decimalResult.value;
-  }
-  return BigInt.fromI32(decimalValue as i32);
+  // try types string for name
+  let nameValue = "unknown";
+  let nameResult = contract.try_name();
+
+  return nameResult.value ? nameResult.value : nameValue;
 }
 
-export function createLiquidityPosition(
-  exchange: Address,
-  user: Address
-): LiquidityPosition {
-  let id = exchange.toHexString().concat("-").concat(user.toHexString());
-  let liquidityTokenBalance = LiquidityPosition.load(id);
-  if (liquidityTokenBalance === null) {
-    let pair = Pair.load(exchange.toHexString());
-    pair.liquidityProviderCount = pair.liquidityProviderCount.plus(ONE_BI);
-    liquidityTokenBalance = new LiquidityPosition(id);
-    liquidityTokenBalance.liquidityTokenBalance = ZERO_BD;
-    liquidityTokenBalance.pair = exchange.toHexString();
-    liquidityTokenBalance.user = user.toHexString();
-    liquidityTokenBalance.save();
-    pair.save();
-  }
-  if (liquidityTokenBalance === null)
-    log.error("LiquidityTokenBalance is null", [id]);
-  return liquidityTokenBalance as LiquidityPosition;
+export function fetchNFTPoolMaxSupply(NFTPoolAddress: Address): BigInt {
+  let contract = NFTPoolContract.bind(NFTPoolAddress);
+
+  let maxSupplyValue = new BigInt(0);
+  let maxSupplyResult = contract.try_maxSupply();
+
+  return maxSupplyResult.value ? maxSupplyResult.value : maxSupplyValue;
+}
+
+export function fetchNFTPoolSeedPrice(NFTPoolAddress: Address): BigInt {
+  let contract = NFTPoolContract.bind(NFTPoolAddress);
+
+  let seedPriceValue = new BigInt(0);
+  let seedPriceResult = contract.try_seedPrice();
+
+  return seedPriceResult.value ? seedPriceResult.value : seedPriceValue;
+}
+
+export function fetchNFTPoolTokenPrice(NFTPoolAddress: Address): BigInt {
+  let contract = NFTPoolContract.bind(NFTPoolAddress);
+
+  let tokenPriceValue = new BigInt(0);
+  let tokenPriceResult = contract.try_tokenPrice();
+
+  return tokenPriceResult.value ? tokenPriceResult.value : tokenPriceValue;
+}
+
+export function fetchNFTPoolTotalSupply(NFTPoolAddress: Address): BigInt {
+  let contract = NFTPoolContract.bind(NFTPoolAddress);
+
+  let totalSupplyValue = new BigInt(0);
+  let totalSupplyResult = contract.try_totalSupply();
+
+  return totalSupplyResult.value ? totalSupplyResult.value : totalSupplyValue;
+}
+
+export function fetchNFTPoolAvailableTokens(NFTPoolAddress: Address): BigInt[] {
+  let contract = NFTPoolContract.bind(NFTPoolAddress);
+
+  let availableTokensValue = [new BigInt(0), new BigInt(0), new BigInt(0), new BigInt(0)];
+  let availableTokensResult = contract.try_getAvailableTokensPerClass();
+
+  return availableTokensResult.value ? [availableTokensResult.value[0], availableTokensResult.value[1], availableTokensResult.value[2], availableTokensResult.value[3]] : availableTokensValue;
 }
 
 export function createUser(address: Address): void {
   let user = User.load(address.toHexString());
-  if (user === null) {
+  if (user === null)
+  {
     user = new User(address.toHexString());
-    user.usdSwapped = ZERO_BD;
+    user.feesEarned = ZERO_BD;
+    user.feesPaid = ZERO_BD;
     user.save();
   }
 }
 
-export function createLiquiditySnapshot(
-  position: LiquidityPosition,
+export function createPoolPosition(
+  poolAddress: Address,
+  userAddress: Address
+): PoolPosition {
+  let id = userAddress.toHexString().concat("-").concat(poolAddress.toHexString());
+  let poolPosition = PoolPosition.load(id);
+  if (poolPosition === null)
+  {
+    poolPosition.tokenBalance = ZERO_BI;
+    poolPosition.averagePrice = ZERO_BI;
+    poolPosition.pool = poolAddress.toHexString();
+    poolPosition.user = userAddress.toHexString();
+    poolPosition.save();
+  }
+
+  return poolPosition as PoolPosition;
+}
+
+export function createNFTPoolPosition(
+  NFTPoolAddress: Address,
+  userAddress: Address
+): NFTPoolPosition {
+  let id = userAddress.toHexString().concat("-").concat(NFTPoolAddress.toHexString());
+  let poolPosition = NFTPoolPosition.load(id);
+  if (poolPosition === null)
+  {
+    poolPosition.tokenBalance = ZERO_BI;
+    poolPosition.averagePrice = ZERO_BI;
+    poolPosition.NFTPool = NFTPoolAddress.toHexString();
+    poolPosition.user = userAddress.toHexString();
+    poolPosition.save();
+  }
+
+  return poolPosition as NFTPoolPosition;
+}
+
+export function createAssetPosition(
+  investmentAddress: Address,
+  tokenAddress: Address,
+  investmentType: number
+): AssetPosition {
+  let id = investmentAddress.toHexString().concat("-").concat(tokenAddress.toHexString());
+  let assetPosition = AssetPosition.load(id);
+  if (assetPosition === null)
+  {
+    assetPosition.tokenBalance = ZERO_BD;
+    assetPosition.tokenAddress = tokenAddress.toHexString();
+
+    if (investmentType == 1)
+    {
+      assetPosition.pool = investmentAddress.toHexString();
+    }
+
+    if (investmentType == 2)
+    {
+      assetPosition.NFTPool = investmentAddress.toHexString();
+    }
+
+    assetPosition.save();
+  }
+
+  return assetPosition as AssetPosition;
+}
+
+export function createPoolPositionSnapshot(
+  position: PoolPosition,
   event: ethereum.Event
 ): void {
   let timestamp = event.block.timestamp.toI32();
-  let pair = Pair.load(position.pair);
-  let token0 = Token.load(pair.token0);
-  let token1 = Token.load(pair.token1);
+  let pool = Pool.load(position.pool);
 
   // create new snapshot
-  let snapshot = new LiquidityPositionSnapshot(
+  let snapshot = new PoolPositionSnapshot(
     position.id.concat(timestamp.toString())
   );
-  snapshot.liquidityPosition = position.id;
+  snapshot.poolPosition = position.id;
   snapshot.timestamp = timestamp;
   snapshot.block = event.block.number.toI32();
   snapshot.user = position.user;
-  snapshot.pair = position.pair;
-  snapshot.token0PriceUSD = token0.derivedCUSD.times(ONE_BD);
-  snapshot.token1PriceUSD = token1.derivedCUSD.times(ONE_BD);
-  snapshot.reserve0 = pair.reserve0;
-  snapshot.reserve1 = pair.reserve1;
-  snapshot.reserveUSD = pair.reserveUSD;
-  snapshot.liquidityTokenTotalSupply = pair.totalSupply;
-  snapshot.liquidityTokenBalance = position.liquidityTokenBalance;
-  snapshot.liquidityPosition = position.id;
+  snapshot.pool = position.pool;
+  snapshot.tokenPrice = pool.tokenPrice;
+  snapshot.totalSupply = pool.totalSupply;
+  snapshot.tokenBalance = position.tokenBalance;
+  snapshot.save();
+  position.save();
+}
+
+export function createNFTPoolPositionSnapshot(
+  position: NFTPoolPosition,
+  event: ethereum.Event
+): void {
+  let timestamp = event.block.timestamp.toI32();
+  let pool = NFTPool.load(position.NFTPool);
+
+  // create new snapshot
+  let snapshot = new NFTPoolPositionSnapshot(
+    position.id.concat(timestamp.toString())
+  );
+  snapshot.NFTPoolPosition = position.id;
+  snapshot.timestamp = timestamp;
+  snapshot.block = event.block.number.toI32();
+  snapshot.user = position.user;
+  snapshot.NFTPool = position.NFTPool;
+  snapshot.tokenPrice = pool.tokenPrice;
+  snapshot.totalSupply = pool.totalSupply;
+  snapshot.tokenBalance = position.tokenBalance;
   snapshot.save();
   position.save();
 }
